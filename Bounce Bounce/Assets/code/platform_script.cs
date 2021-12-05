f88b54de038df337009281c39194b33c35b3f6a6
EthanXuXu <ethanx4321@gmail.com>
2021-12-05 12:03:06 -0500

    added functionality to grow and shrink power-ups

--- Bounce Bounce/Assets/code/platform_script.cs
+++ Bounce Bounce/Assets/code/platform_script.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class platform_script : MonoBehaviour
 {
     public float speed = 10f;
+
+    private static float size_change_time;
+    private static float speed_change_time;
     private Vector3 large_size = new Vector3(4, 0.5f, 4);
     private Vector3 normal_size = new Vector3(3, 0.5f, 3);
     private Vector3 small_size = new Vector3(2, 0.5f, 2);
@@ -14,6 +17,7 @@ public class platform_script : MonoBehaviour
     void Start()
     {
         speed = 10f;
+        size_change_time = 0f;
     }
 
     // Update is called once per frame
@@ -26,17 +30,26 @@ public class platform_script : MonoBehaviour
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, -9f + (size.x/2), 9f - (size.x/2)), 
             transform.position.y, 
             Mathf.Clamp(transform.position.z, -9.5f + (size.z/2), 9.5f - (size.z/2)));
+        
+        //Revert to normal size after 5 seconds of size change
+        if(Time.time - size_change_time >= 5f){
+            turn_normal();
+        }
 
-        Debug.Log("Size " + size);
-        Debug.Log("Z position " + transform.position.z);
-        Debug.Log("9.5 - (size.z/2) = " + (9.5 - size.z/2));
+        //Revert to normal speed after 5 seconds of speed change
+        if(Time.time - size_change_time >= 5f){
+            turn_normal();
+        }
+
         if(Input.GetKey("up") && transform.position.z < 9.45 - (size.z/2))
         {
+            speed_up();
             transform.Translate(new Vector3(0,0,1) * speed * Time.deltaTime);
         }
 
         if(Input.GetKey("down") && transform.position.z > -9.45 + (size.z/2))
         {
+           
             transform.Translate(new Vector3(0,0,-1) * speed * Time.deltaTime);
         }
 
@@ -50,5 +63,28 @@ public class platform_script : MonoBehaviour
             transform.Translate(new Vector3(1,0,0) * speed * Time.deltaTime);
         }
 
+    }
+
+    public void turn_small(){
+        size_change_time = Time.time;
+        transform.localScale = small_size;
+    }
+
+    public void turn_normal(){
+        transform.localScale = normal_size;
+    }
+    
+    public void turn_large(){
+        size_change_time = Time.time;
+        transform.localScale = large_size;
+    }
+
+    public void speed_up(){
+        speed_change_time = Time.time;
+        speed = 15f;
+    }
+
+    public void normal_speed(){
+        speed = 10f;
     }
 }
---
+++ Bounce Bounce/Assets/code/shrink_script.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shrink_script : MonoBehaviour
+{
+
+    private GameObject platform;
+    private platform_script plat_script;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platform = GameObject.FindWithTag("platform_small");
+        plat_script = platform.GetComponent<platform_script>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "platform_small")
+        {
+            plat_script.turn_small();
+            Destroy(gameObject);
+        }
+    }
+}
