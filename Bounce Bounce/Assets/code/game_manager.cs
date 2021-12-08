39e00c5bcf7730d7887fa19035079c8b701787e8
EthanXuXu <ethanx4321@gmail.com>
2021-12-08 18:00:58 -0500

    Update game_manager.cs

--- Bounce Bounce/Assets/code/game_manager.cs
+++ Bounce Bounce/Assets/code/game_manager.cs
@@ -12,12 +12,25 @@ public class game_manager : MonoBehaviour
     private float ball_speed; 
     private static int lives;
 
+    //Variables for spawning new balls
+    private Vector3 prev_position;
+    private Vector3 new_position;
+    private int xPos;
+    private int zPos;
+    private float time_diff;
+    private float prev_time;
+    public GameObject ball_prefab;
+
+
     // Start is called before the first frame update
     void Start()
     {
         lives = 10;
         score = 0;
         ball_speed = 3f;
+        prev_position = new Vector3(0, 9, 0);
+        prev_time = 0f;
+        calc_new_ball_pos();
     }
 
     // Update is called once per frame
@@ -27,6 +40,20 @@ public class game_manager : MonoBehaviour
             Application.Quit();
         }
         Debug.Log("Score " + score);
+
+        if(Time.time - prev_time >= time_diff){
+            Instantiate(ball_prefab, new_position, Quaternion.identity);
+            prev_time = Time.time;
+            calc_new_ball_pos();
+        }
+    }
+
+    public void calc_new_ball_pos(){
+        xPos = Random.Range(-7, 7);
+        zPos = Random.Range(-8, 8);
+        new_position = new Vector3(xPos, 9, zPos);
+        time_diff = Vector3.Distance(prev_position, new_position) / 10f + 0.1f;
+        prev_position = new_position;
     }
 
     public void increase_score(){
