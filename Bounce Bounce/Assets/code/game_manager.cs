a98f2f450f6f9a3dc5a5b6d2a28538966aa26a25
EthanXuXu <ethanx4321@gmail.com>
2021-12-08 08:38:28 -0500

    beginning spawn algorithm

--- Bounce Bounce/Assets/code/game_manager.cs
+++ Bounce Bounce/Assets/code/game_manager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class game_manager : MonoBehaviour
 {
+    (float, float) x_coords = (-7f, 7f);
+    (float, float) z_coords = (-8f, 8f);
+
     private static int score;
     private float ball_speed; 
     // Start is called before the first frame update
