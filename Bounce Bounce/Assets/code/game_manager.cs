dacc515272847b872b5a1fe232586f619418cf07
EthanXuXu <ethanx4321@gmail.com>
2021-12-08 19:43:47 -0500

    finished spawn algorithm

--- Bounce Bounce/Assets/code/game_manager.cs
+++ Bounce Bounce/Assets/code/game_manager.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class game_manager : MonoBehaviour
 {
-    (float, float) x_coords = (-7f, 7f);
-    (float, float) z_coords = (-8f, 8f);
+    // (float, float) x_coords = (-7f, 7f);
+    // (float, float) z_coords = (-8f, 8f);
 
     private float ball_spawn_time; 
     private static int score;
     private float ball_speed; 
+    private float powerup_speed;
     private static int lives;
 
     //Variables for spawning new balls
@@ -21,6 +22,13 @@ public class game_manager : MonoBehaviour
     private float prev_time;
     public GameObject ball_prefab;
 
+    //Variables for spawning new power-ups
+    public GameObject shrink_prefab;
+    public GameObject grow_prefab;
+    public GameObject bomb_prefab;
+    public GameObject speed_prefab;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +36,7 @@ public class game_manager : MonoBehaviour
         lives = 10;
         score = 0;
         ball_speed = 3f;
+        powerup_speed = 2f;
         prev_position = new Vector3(0, 9, 0);
         prev_time = 0f;
         calc_new_ball_pos();
@@ -42,7 +51,12 @@ public class game_manager : MonoBehaviour
         Debug.Log("Score " + score);
 
         if(Time.time - prev_time >= time_diff){
-            Instantiate(ball_prefab, new_position, Quaternion.identity);
+            //One in ten chance of spawning a powerup
+            if(Random.Range(1, 10) == 1) {
+                spawn_powerup(Random.Range(1,5));
+            } else {
+                Instantiate(ball_prefab, new_position, Quaternion.identity);
+            }
             prev_time = Time.time;
             calc_new_ball_pos();
         }
@@ -52,8 +66,24 @@ public class game_manager : MonoBehaviour
         xPos = Random.Range(-7, 7);
         zPos = Random.Range(-8, 8);
         new_position = new Vector3(xPos, 9, zPos);
-        time_diff = Vector3.Distance(prev_position, new_position) / 10f + 0.1f;
+        time_diff = Vector3.Distance(prev_position, new_position) / 10f + 0.2f;
         prev_position = new_position;
+    }
+
+    public void spawn_powerup(int powerup_num){
+        if(powerup_num == 1)
+        {
+            Instantiate(grow_prefab, new_position, Quaternion.identity);
+        } else if (powerup_num == 2) 
+        {
+            Instantiate(shrink_prefab, new_position, Quaternion.identity);
+        } else if (powerup_num == 3) 
+        {
+            Instantiate(bomb_prefab, new_position, Quaternion.identity);
+        } else
+        {
+            Instantiate(speed_prefab, new_position, Quaternion.identity);
+        }
     }
 
     public void increase_score(){
@@ -74,5 +104,9 @@ public class game_manager : MonoBehaviour
 
     public void decrease_lives(){
         lives -= 1;
+    }
+
+    public float get_powerup_speed(){
+        return powerup_speed;
     }
 }
--- Bounce Bounce/Assets/code/speed_script.cs
+++ Bounce Bounce/Assets/code/speed_script.cs
@@ -6,16 +6,27 @@ public class speed_script : MonoBehaviour
 {
     private GameObject platform;
     private platform_script plat_script;
+
+    private GameObject game_manager;
+    private game_manager manager_script;
+
+    private float speed;
     // Start is called before the first frame update
     void Start()
     {
         platform = GameObject.FindWithTag("platform_small");
         plat_script = platform.GetComponent<platform_script>();
+        
+        game_manager = GameObject.FindWithTag("game_manager");
+        manager_script = game_manager.GetComponent<game_manager>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        speed = manager_script.get_powerup_speed();
+        transform.Translate(new Vector3(0,-1,0) * speed * Time.deltaTime);
+        
         //If the shrink power up falls 2 meters below the platform its destroyed
         if(transform.position.y <= -2){
             Destroy(gameObject);
